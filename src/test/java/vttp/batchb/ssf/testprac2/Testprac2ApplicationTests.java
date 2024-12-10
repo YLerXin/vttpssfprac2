78b3ea4fb2a30a23bea701b44593dc5d6e86ef01
Yong_Ler_Xin <yonglerxin@gmail.com>
2024-12-10 16:16:14 +0800

    initial commit

---
+++ src/test/java/vttp/batchb/ssf/testprac2/Testprac2ApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.batchb.ssf.testprac2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testprac2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
